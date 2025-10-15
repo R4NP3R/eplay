cfe9623548c90bff00b38b6f65b7a43e0fe877df
R4NP3R <ssrafa.2014@gmail.com>
2025-10-15 17:27:38 -0300

    alterando o link da API

--- src/services/api.ts
+++ src/services/api.ts
@@ -40,7 +40,7 @@ type PurchaseResponse = {
 
 const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://fake-api-tau.vercel.app/api/eplay/'
+    baseUrl: 'https://api-ebac.vercel.app/api/eplay/'
   }),
   endpoints: (builder) => ({
     getFeaturedGames: builder.query<Game, void>({
